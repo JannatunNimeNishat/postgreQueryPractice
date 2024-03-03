daec601cd85449668687d3e61aa7b37029ba0ebf
JannatunNimeNishat <nishatnime100@gmail.com>
2024-03-03 21:24:15 +0600

    functionProcedures.sql added

---
+++ functionProcedures.sql
@@ -0,0 +1,45 @@
+-- Active: 1709106603147@@127.0.0.1@5432@ph@public
+
+select * from employees;
+
+-------20-5 Exploring Functions In PostgreSQL---------------
+--creating a function
+CREATE Function emp_count()
+RETURNS INT
+LANGUAGE SQL
+as
+$$
+    SELECT count(*) FROM employees;
+$$;
+
+---- calling the function
+SELECT emp_count();
+
+
+
+--- deleting function with void return type
+create or REPLACE FUNCTION delete_emp()
+RETURNS void
+LANGUAGE SQL
+as
+$$
+    DELETE from employees WHERE employee_id = 30;
+$$
+
+
+SELECT delete_emp();
+
+
+------------------------ function with parameter -------------------
+create or REPLACE FUNCTION delete_emp_by_id(p_emp_id int)
+RETURNS void
+LANGUAGE SQL
+as 
+$$
+    DELETE FROM employees WHERE employee_id = p_emp_id;
+$$;
+
+SELECT delete_emp_by_id(29);
+
+
+
